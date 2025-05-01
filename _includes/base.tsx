55991023fa4e3318e866e79383dc8bee11233170
Frog Chen <satgo1546@yeah.net>
2025-05-01 21:13:02 +0800

    添加文章602

--- _includes/base.tsx
+++ _includes/base.tsx
@@ -1,3 +1,11 @@
+function htmlToPlainText(x: string): string {
+	x = x.replace(/<[^>]*>/g, '')
+	// This does not cover everything, but I do not write fancy entities in title and description anyway.
+	x = x.replaceAll('&lt;', '<').replaceAll('&gt;', '>')
+	x = x.replaceAll('&amp;', '&')
+	return x
+}
+
 export default (data: Lume.Data) => {
 	return <html lang="zh-Hans" class={data.theme}>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -6,9 +14,9 @@ export default (data: Lume.Data) => {
 			data.url === '/'
 				? <title>satgo1546’s ocean</title>
 				: <>
-					<title>{data.title.replace(/<[^>]*>/g, '')} · satgo1546’s ocean</title>
+					<title>{htmlToPlainText(data.title!)} · satgo1546’s ocean</title>
 					{!!data.tags.length && <meta name="keywords" content={data.tags.join()} />}
-					{data.description && <meta name="description" content={data.description.replace(/<[^>]*>/g, '')} />}
+					{data.description && <meta name="description" content={htmlToPlainText(data.description)} />}
 				</>
 		}
 		<meta name="generator" content={data.generator} />
